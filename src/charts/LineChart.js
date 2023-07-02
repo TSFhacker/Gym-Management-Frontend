f0d23257373ce5f80d8a0f519457d6c66542bf1b
TSFhacker <dungbui1110@gmail.com>
2023-07-03 01:18:06 +0700

    configuring dashboard

--- src/charts/LineChart.js
+++ src/charts/LineChart.js
@@ -1,45 +1,48 @@
-import React from 'react';
-import { Line } from 'react-chartjs-2';
-import { Chart as ChartJs } from 'chart.js/auto';
-
+import React from "react";
+import { Line } from "react-chartjs-2";
+import { Chart as ChartJs } from "chart.js/auto";
 
 const LineChart = () => {
+  const data = {
+    labels: ["January", "February", "March", "April", "May", "June"],
+    datasets: [
+      {
+        label: "millions VND",
+        data: [50, 50, 70, 60, 40, 30],
+        backgroundColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
 
-    const data = {
-        labels: ['Apple', 'Dell', 'HP', 'Samsung'],
-        datasets: [{
-            label: '# of products',
-            data: [5, 5, 7, 6],
-            backgroundColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)'
-            ],
-            borderWidth: 1
-        }]
-    };
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
 
-    const options = {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-
-    return (
-        <div>
-            <Line data={data} options={options} />
-        </div>
-    );
+  return (
+    <div>
+      <Line data={data} options={options} />
+    </div>
+  );
 };
-
 
 export default LineChart;
--- src/components/dashboard/DashboardContent.jsx
+++ src/components/dashboard/DashboardContent.jsx
@@ -1,72 +1,97 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-import { FaWallet } from 'react-icons/fa'
-import BarChart from '../../charts/BarChart';
-import LineChart from '../../charts/LineChart';
-import DoughnutChart from '../../charts/DoughnutChart';
-import RadarChart from '../../charts/RadarChart';
-
-
+import { FaWallet } from "react-icons/fa";
+import BarChart from "../../charts/BarChart";
+import LineChart from "../../charts/LineChart";
+import DoughnutChart from "../../charts/DoughnutChart";
+import RadarChart from "../../charts/RadarChart";
 
 const DashboardContent = () => {
-    return (
-        <div className=''>
-            {/* start banner */}
-            <div className='bg-indigo-200 m-7 p-6'>
-                <div className='text-4xl text-gray-800 font-medium'>Good Moring, Administrator.😃</div>
-                <div className='mt-4'>These are the stats for today, have a good day.</div>
-            </div>
-            {/* end banner */}
+  const [registrationList, setRegistrationList] = useState([]);
+  const [thisMonthProfit, setThisMonthProfit] = useState(0);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-            <div className='flex m-7 space-x-6'>
-                <div className='w-1/2 bg-white p-4'>
-                    <span className='text-amber-400 text-2xl'><FaWallet /></span>
-                    <h2 className="font-bold text-xl text-gray-700">Tech shop</h2>
-                    <h3 className="uppercase my-3 font-bold text-sm text-gray-400">products</h3>
-                    <div className='flex items-center'>
-                        <span className="font-bold text-3xl mr-2">$24,780</span>
-                        <span className="bg-green-500 rounded-full p-1 text-white text-sm font-bold align-text-top">+49%</span>
-                    </div>
-                    <BarChart />
-                </div>
-                <div className='w-1/2 bg-white p-4'>
-                    <span className='text-amber-400 text-2xl'><FaWallet /></span>
-                    <h2 className="font-bold text-xl text-gray-700">Tech shop</h2>
-                    <h3 className="uppercase my-3 font-bold text-sm text-gray-400">products</h3>
-                    <div className='flex items-center'>
-                        <span className="font-bold text-3xl mr-2">$24,780</span>
-                        <span className="bg-red-500 rounded-full p-1 text-white text-sm font-bold align-text-top">-9%</span>
-                    </div>
-                    <LineChart />
-                </div>
-            </div>
+  const fetchData = async () => {
+    let result = await fetch("http://localhost:8080/api/registrations");
+    result = await result.json();
+    setRegistrationList(result);
+  };
 
-
-            <div className='flex m-7 space-x-6'>
-                <div className='w-1/2 bg-white p-4'>
-                    <span className='text-amber-400 text-2xl'><FaWallet /></span>
-                    <h2 className="font-bold text-xl text-gray-700">Tech shop</h2>
-                    <h3 className="uppercase my-3 font-bold text-sm text-gray-400">products</h3>
-                    <div className='flex items-center'>
-                        <span className="font-bold text-3xl mr-2">$24,780</span>
-                        <span className="bg-green-500 rounded-full p-1 text-white text-sm font-bold align-text-top">+49%</span>
-                    </div>
-                    <DoughnutChart />
-                </div>
-                <div className='w-1/2 bg-white p-4'>
-                    <span className='text-amber-400 text-2xl'><FaWallet /></span>
-                    <h2 className="font-bold text-xl text-gray-700">Tech shop</h2>
-                    <h3 className="uppercase my-3 font-bold text-sm text-gray-400">products</h3>
-                    <div className='flex items-center'>
-                        <span className="font-bold text-3xl mr-2">$24,780</span>
-                        <span className="bg-red-500 rounded-full p-1 text-white text-sm font-bold align-text-top">-9%</span>
-                    </div>
-                    <RadarChart />
-                </div>
-            </div>
+  return (
+    <div className="">
+      {/* start banner */}
+      <div className="bg-indigo-200 m-7 p-6">
+        <div className="text-4xl text-gray-800 font-medium">
+          Good Moring, Administrator.😃
         </div>
-    );
-};
+        <div className="mt-4">
+          These are the stats for today, have a good day.
+        </div>
+      </div>
+      {/* end banner */}
 
+      <div className="flex m-7 space-x-6">
+        <div className="w-1/2 bg-white p-4">
+          <span className="text-amber-400 text-2xl">
+            <FaWallet />
+          </span>
+          <h2 className="font-bold text-xl text-gray-700">Gym Management</h2>
+          <h3 className="uppercase my-3 font-bold text-sm text-gray-400">
+            Current number of subscriptions
+          </h3>
+          <div className="flex items-center">
+            <span className="font-bold text-3xl mr-2">48 people</span>
+          </div>
+          <BarChart />
+        </div>
+        <div className="w-1/2 bg-white p-4">
+          <span className="text-amber-400 text-2xl">
+            <FaWallet />
+          </span>
+          <h3 className="uppercase my-3 font-bold text-sm text-gray-400">
+            This year's profit
+          </h3>
+          <div className="flex items-center">
+            <span className="font-bold text-3xl mr-2">300 millions VND</span>
+            <span className="bg-red-500 rounded-full p-1 text-white text-sm font-bold align-text-top">
+              -25%
+            </span>
+          </div>
+          <LineChart />
+        </div>
+      </div>
+
+      <div className="flex m-7 space-x-6">
+        <div className="w-1/2 bg-white p-4">
+          <span className="text-amber-400 text-2xl">
+            <FaWallet />
+          </span>
+          <h3 className="uppercase my-3 font-bold text-sm text-gray-400">
+            Facility
+          </h3>
+          <DoughnutChart />
+        </div>
+        {/* <div className="w-1/2 bg-white p-4">
+          <span className="text-amber-400 text-2xl">
+            <FaWallet />
+          </span>
+          <h2 className="font-bold text-xl text-gray-700">Tech shop</h2>
+          <h3 className="uppercase my-3 font-bold text-sm text-gray-400">
+            products
+          </h3>
+          <div className="flex items-center">
+            <span className="font-bold text-3xl mr-2">$24,780</span>
+            <span className="bg-red-500 rounded-full p-1 text-white text-sm font-bold align-text-top">
+              -9%
+            </span>
+          </div>
+          <RadarChart />
+        </div> */}
+      </div>
+    </div>
+  );
+};
 
 export default DashboardContent;
